0cdd0e4abc44f622a6f1af9c5bdee5e1d5a83aa2
Alex Buznik <shu@buznik.net>
2019-08-07 01:13:14 +0300

    Comment all Serial.print except for the critical ones

--- Arduino/player1/player1.ino
+++ Arduino/player1/player1.ino
@@ -1,19 +1,8 @@
 /*
+  Plastic Player Arduino Controller
 
-* PN532 NFC RFID Module (v3)
-
-* NFC Tag Quick Test Code v1
-
-* Based on an example code from PN532 Arduino Library
-
-* Tailored for I2C & Arduino Uno
-
-* Remember to set mode switch of PN532 module to I2C!
-
-* T.K.Hareendran/2019
-
-* www.electroschematics.com
-
+  Alex Buznik <shu@buznik.net>
+  2019
 */
 #include <SPI.h>
 #include <Wire.h>
@@ -67,7 +56,7 @@ unsigned long currentMillis;
  * format: text/plain
  * data: wifi&[ssid name]&[password]
  */
-int nfcDelay = 1000;
+int nfcDelay = 500;
 String nfcPtrTmp;
 String nfcCardId;
 String nfcCardWifi;
@@ -78,11 +67,11 @@ void setup(void) {
   startMillis = millis();
   nfc.begin();
 
-  Serial.println("NFC began");
+  // Serial.println("NFC began");
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
+    // Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
   // Show initial display buffer contents on the screen --
@@ -92,60 +81,60 @@ void setup(void) {
 }
 
 void loop(void) {
-  scanNfc(); 
+  scanNfc();
   delay(nfcDelay);
 }
 
 void scanNfc() {
-  Serial.println("\nScan a NFC tag\n");
+  // Serial.println("\nScan a NFC tag\n");
   nfcPtrTmp = "";
   nfcCardId = "";
   nfcCardWifi = "";
   if (nfc.tagPresent())
   {
     NfcTag tag = nfc.read();
-    tag.print();
+    // tag.print();
     nfcCardId = tag.getUidString();
-  
+
     if (tag.hasNdefMessage())
     {
       NdefMessage message = tag.getNdefMessage();
-      
+
       // If you have more than 1 Message then it wil cycle through them
       int recordCount = message.getRecordCount();
       for (int i = 0; i < recordCount; i++)
       {
-        
+
         NdefRecord record = message.getRecord(i);
 
         int payloadLength = record.getPayloadLength();
         byte payload[payloadLength];
         record.getPayload(payload);
-        
+
         String payloadAsString = ""; // Processes the message as a string vs as a HEX value
-        for (int c = 0; c < payloadLength; c++) 
+        for (int c = 0; c < payloadLength; c++)
         {
           payloadAsString += (char)payload[c];
         }
-        
-        Serial.println("\nTag Content Shown Below\n");
-        
-        Serial.println("Original String: ");
-        Serial.println(payloadAsString);
+
+        // Serial.println("\nTag Content Shown Below\n");
+
+        // Serial.println("Original String: ");
+        // Serial.println(payloadAsString);
 
         // Detect WI-FI
         int init_size = payloadLength;
         String strCopy = payloadAsString;
         char delim[] = "&";
         nfcCardWifi = "";
-      
+
         char *ptr = strtok(strCopy.c_str(), delim);
-      
+
         for (int i = 0; i < init_size; i++)
         {
           nfcPtrTmp = String(ptr);
-          Serial.println("token: " + nfcPtrTmp);
-          Serial.println("token is wifi: " + nfcPtrTmp.equals("wifi"));
+          // Serial.println("token: " + nfcPtrTmp);
+          // Serial.println("token is wifi: " + nfcPtrTmp.equals("wifi"));
           ptr = strtok(NULL, delim);
 
           // it's a wifi card!
@@ -160,15 +149,15 @@ void scanNfc() {
         }
 
         String uid = record.getId();
-        if (uid != "") 
+        if (uid != "")
         {
-          Serial.print("  ID: ");
-          Serial.println(uid);
+          // Serial.print("  ID: ");
+          // Serial.println(uid);
         }
       }
-      
+
     }
-    
+
     if (nfcCardWifi.length()) {
       drawText("WI-FI Card Detected...");
       msgComputer(nfcCardWifi);
@@ -184,7 +173,6 @@ String formatNfcCardId(String id) {
 }
 
 void msgComputer(String data) {
-  Serial.println("msgComputer");
   Serial.println(data);
 }
 
@@ -201,7 +189,7 @@ void drawText(String text) {
   display.setCursor(0, 0);
   display.println(text);
   display.display();      // Show initial text
-  delay(100); 
+  delay(100);
 }
 
 void drawScrolltext(String text) {
@@ -227,4 +215,5 @@ void scrollTextLeftRight(void) {
   display.stopscroll();
   scrollTextLeftRight();
 }
+
 
